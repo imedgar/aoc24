bf3740de25bc0ed222f24c99b5dcd41fa294893d
imedgar <ed.ga.ru90@gmail.com>
2024-12-09 21:19:13 +0200

    day8 part2 wip

--- day8/day8.go
+++ day8/day8.go
@@ -45,6 +45,15 @@ func Day8() {
 		area = append(area, floor)
 		l++
 	}
+	// part1
+	part2()
+	fmt.Println("total antinodes", total)
+	for _, f := range area {
+		fmt.Println(f)
+	}
+}
+
+func part1() {
 	// pair antennas
 	for k := range ant {
 		for _, curr := range ant[k] {
@@ -57,32 +66,67 @@ func Day8() {
 					// fmt.Println("antinode out of borders")
 					continue
 				}
-				// fmt.Println("trying antinode for", curr.x, curr.y, "to", a.x, a.y, "at", antiX, antiY)
-				potential := area[antiX][antiY]
-				if potential == "." {
-					//					fmt.Println("current", curr.x, curr.y, "pairing to", a.x, a.y, "at", antiX, antiY)
-					area[antiX][antiY] = "#"
-					key := fmt.Sprintf("%s-%s", antiX, antiY)
-					if !unique.Contains(key) {
-						unique.Add(fmt.Sprintf("%s-%s", antiX, antiY))
-						total++
-					}
-				} else if potential != "#" {
-					//				fmt.Println("current", curr.x, curr.y, "pairing to", a.x, a.y, "diff", antiX, antiY, "overlaps antenna")
-					key := fmt.Sprintf("%s-%s", antiX, antiY)
-					if !unique.Contains(key) {
-						unique.Add(fmt.Sprintf("%s-%s", antiX, antiY))
-						total++
-					}
-				} else if potential == "#" {
-					//			fmt.Println("current", curr.x, curr.y, "pairing to", a.x, a.y, "diff", antiX, antiY, "overlaps antinode")
-					// total++
-				}
+				addAntinode(antiX, antiY)
 			}
 		}
 	}
-	fmt.Println("total antinodes", total)
-	//	for _, f := range area {
-	//		fmt.Println(f)
-	//	}
+}
+
+func part2() {
+	// pair antennas
+	for k := range ant {
+		for _, curr := range ant[k] {
+			for _, a := range ant[k] {
+				if curr.x == a.x && curr.y == a.y {
+					continue
+				}
+				antiX, antiY := a.x-(curr.x-a.x), a.y-(curr.y-a.y)
+				if antiX < 0 || antiX > len(area)-1 || antiY < 0 || antiY > len(area[antiX])-1 {
+					// fmt.Println("antinode out of borders")
+					continue
+				}
+				addAntinodes(curr.x, curr.y, a.x, a.y)
+			}
+		}
+	}
+}
+
+func addAntinode(antiX, antiY int) {
+	// fmt.Println("trying antinode for", curr.x, curr.y, "to", a.x, a.y, "at", antiX, antiY)
+	potential := area[antiX][antiY]
+	if potential == "." {
+		area[antiX][antiY] = "#"
+		key := fmt.Sprintf("%s-%s", antiX, antiY)
+		if !unique.Contains(key) {
+			fmt.Println("antinode at", antiX, antiY)
+			unique.Add(fmt.Sprintf("%s-%s", antiX, antiY))
+			total++
+		}
+	} else if potential != "#" {
+		key := fmt.Sprintf("%s-%s", antiX, antiY)
+		if !unique.Contains(key) {
+			fmt.Println("current", antiX, antiY, "overlaps antenna")
+			unique.Add(fmt.Sprintf("%s-%s", antiX, antiY))
+			total++
+		}
+	} else if potential == "#" {
+		//			fmt.Println("current", curr.x, curr.y, "pairing to", a.x, a.y, "diff", antiX, antiY, "overlaps antinode")
+		// total++
+	}
+}
+
+func addAntinodes(prevX, prevY, newX, newY int) {
+	// fmt.Println("trying antinode for", curr.x, curr.y, "to", a.x, a.y, "at", antiX, antiY)
+	subsX, subsY := prevX-newX, prevY-newY
+	antiX, antiY := newX-subsX, newY-subsY
+	if antiX < 0 || antiX > len(area)-1 || antiY < 0 || antiY > len(area[antiX])-1 {
+		fmt.Println("antinode out of borders", antiX, antiY)
+		return
+	}
+	for antiX >= 0 && antiX <= len(area)-1 && antiY >= 0 && antiY <= len(area[antiX])-1 {
+		fmt.Println("adding at", antiX, antiY)
+		addAntinode(antiX, antiY)
+		antiX, antiY = antiX-subsX, antiY-subsY
+	}
+	fmt.Println("finished adding at", antiX, antiY)
 }
