9cf2fa62d5c3aad848a4b734e897918e9c920cc8
imedgar <ed.ga.ru90@gmail.com>
2024-12-16 10:06:26 +0100

    day 10 part 2 im late a f but gonna try anyway :(

--- day10/day10.go
+++ day10/day10.go
@@ -53,7 +53,12 @@ func Day10() {
 		trailhead := checkTrail(th)
 		totalScore += trailhead.score
 	}
-
+	fmt.Printf("Total score of all trailheads: %d\n", totalScore)
+	totalScore = 0
+	for _, th := range trailheads {
+		trailhead := checkTrailRating(th)
+		totalScore += trailhead.score
+	}
 	fmt.Printf("Total score of all trailheads: %d\n", totalScore)
 }
 
@@ -73,7 +78,6 @@ func checkTrail(th Trailhead) Trailhead {
 			continue
 		}
 		visited[key] = true
-
 		if mountain[x][y] == 9 {
 			score++
 			continue
@@ -97,23 +101,46 @@ func checkTrail(th Trailhead) Trailhead {
 	return th
 }
 
-func checkSurroundings(th Trailhead, tar int) [][]int {
-	rows := len(mountain)
-	cols := len(mountain[0])
-	next := [][]int{}
-	for _, dir := range directions {
-		newRow := th.x + dir[0]
-		newCol := th.y + dir[1]
-		if newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols {
-			val := mountain[newRow][newCol]
-			if val == tar {
-				next = append(next, []int{newRow, newCol})
+func getKey(th Trailhead) string {
+	return fmt.Sprintf("%d-%d", th.x, th.y)
+}
+
+func checkTrailRating(th Trailhead) Trailhead {
+	visited := make(map[string]bool)
+	queue := [][]int{{th.x, th.y}}
+	rows, cols := len(mountain), len(mountain[0])
+	score := 0
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		x, y := current[0], current[1]
+		key := fmt.Sprintf("%d-%d", x, y)
+		if visited[key] {
+			continue
+		}
+		visited[key] = true
+		if mountain[x][y] == 9 {
+			visited = make(map[string]bool)
+			score++
+			continue
+		}
+
+		nextLvl := mountain[x][y] + 1
+		for _, dir := range directions {
+			newX, newY := x+dir[0], y+dir[1]
+			if newX >= 0 && newX < rows && newY >= 0 && newY < cols {
+				if mountain[newX][newY] == nextLvl {
+					nKey := fmt.Sprintf("%d-%d", newX, newY)
+					if !visited[nKey] {
+						queue = append([][]int{{newX, newY}}, queue...)
+					}
+				}
 			}
 		}
 	}
-	return next
-}
 
-func getKey(th Trailhead) string {
-	return fmt.Sprintf("%d-%d", th.x, th.y)
+	th.score = score
+	return th
 }
